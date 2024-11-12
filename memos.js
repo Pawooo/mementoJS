16f8296e43960e483a4b41ef56c3585aeafd9ce2
Andrian <shemberko.andrian@gmail.com>
2024-11-12 13:07:21 +0900

    arrays and immutability

--- memos.js
+++ memos.js
@@ -49,3 +49,47 @@ fetch(api-URL)
 const getStarWarsPerson = async () => {
     const res = await axios.get("https://swapi.dev/api/people/1")
 }
+
+// Setting the state once even on rerender
+// tl;dr
+// This will render once and NOT reset the state to generateGameBoard return value
+const [board, setBoard] = useState(generateGameBoard); 
+// This will reset the state on rerender
+const [board, setBoard] = useState(generateGameBoard()); 
+
+function generateGameBoard() {
+    console.log("MAKING THE INITIAL GAME BOARD");
+    return Array(5000);
+}
+
+export default function Dumbo() {
+    const [board, setBoard] = useState(generateGameBoard);
+    return (
+        <button onClick={() => setBoard("hello")}>Click me to set state to something else</button>
+    ) 
+}
+
+// REACT WILL RERENDER EVERY TIME A SIMPLE STATE IS UPDATED
+// IT WILL NOT RERENDER EVERY TIME MUTABLES (OBJECTS, ARRAYS) ARE UPDATED, BECAUSE IN THE MEMORY IT THINKS IT'S THE SAME
+// You need to pass an arr/obj to a state to update it, which is why in React people copy the old one and modify specific parts of the copy
+
+// Common React Array Patterns
+// ADDING TO AN ARRAY
+[...shoppingCart, {id: 4, product: "Coffee Mug", price: 7.99}];
+// REMOVING AN ELEMENT
+shoppingCart.filter((item) => item.id !== 2);
+// UPDATING ALL ELEMENTS IN AN ARRAY
+shoppingCart.map((item) => {
+    return {
+        ...item,
+        product: item.product.toLowerCase(),
+    }
+})
+// MODIFYING A PARTICULAR ELEMENT IN AN ARRAY
+shoppingCart.map((item => {
+    if (item.id === 3) {
+        return {...item, price: 10.99}
+    } else {
+        return item;
+    }
+}))
